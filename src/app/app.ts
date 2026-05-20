f2ef0932ac1836ed98431706e969165c4d87fa90
prxsss <9phuriphat.k@gmail.com>
2026-05-20 11:51:02 +0700

    refactor: remove unused files

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,6 @@ import { ButtonModule } from 'primeng/button';
   selector: 'app-root',
   imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('inventory-ui');
